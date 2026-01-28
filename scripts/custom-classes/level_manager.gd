acf65603bab3ca33b493d6ade23831f678d16bc8
Nicolas Kuhn de Chizelle <nk436@cornell.edu>
2026-01-27 16:34:45 -0800

    adding nico test level

--- scripts/custom-classes/level_manager.gd
+++ scripts/custom-classes/level_manager.gd
@@ -4,7 +4,7 @@ class_name LevelManager
 extends Node
 
 const LEVEL_DIRECTORY: String = "res://scenes/levels/"
-const level_order: Array[String] = ["1", "2"]
+const level_order: Array[String] = ["nico-test", "1", "2"]
 
 var _current_level_index: int = 0
 var _current_level: Node2D = null
